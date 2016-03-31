328192d62c346706b139d4199916a59a36be0462
Victor <victor.dupuy@hei.fr>
2016-03-30 20:41:29 -0400

    Adding some fake data

---
+++ server/databases/users.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    name : "Victor Dupuy",
+    distance : 10,
+    latitude : 45.5185914,
+    longitude : -73.5570006,
+    location : "1432 rue Moncalm",
+    rating : 4,
+    img : "https://scontent-yyz1-1.xx.fbcdn.net/hprofile-xap1/v/t1.0-1/p160x160/12439224_10206854442013813_7037859228649959634_n.jpg?oh=85f2d71b1d74f2e63b4e7cdd5ed37724&oe=57830260"
+  },
+  {
+    name : "Etienne Phelip",
+    distance : 10,
+    latitude : 45.5185914,
+    longitude : -73.5570006,
+    location : "1432 rue Moncalm",
+    rating : 4,
+    img : "https://scontent-yyz1-1.xx.fbcdn.net/hprofile-xfp1/v/t1.0-1/p160x160/12642832_10153678094598889_4705523671150478590_n.jpg?oh=778554973401c086ac423e061dd548e3&oe=57990145"
+  },
+  {
+    name : "Maëlle Gourgues",
+    distance : 10,
+    lat : 45.5355688,
+    long : -73.60456479999999,
+    location : "Vers Beaubien ! ",
+    rating : 4,
+    img : "https://scontent-yyz1-1.xx.fbcdn.net/hprofile-xfa1/v/t1.0-1/p160x160/12651211_10153943230517372_2130762525473858167_n.jpg?oh=ee6576b2f09b1ccd648ff60df7219b69&oe=5791897E"
+  },
+  {
+    name : "Pauline Savey",
+    distance : 10,
+    lat : 45.5311995,
+    long : -73.59775939999997,
+    location : "Vers Rosemont !",
+    rating : 4,
+    img : "https://scontent-yyz1-1.xx.fbcdn.net/hprofile-xlp1/v/t1.0-1/p160x160/9168_10154010472791552_8287602012123052526_n.jpg?oh=e5ab6f15663d9b8edd0916d5d2d593a9&oe=57944202"
+  }
+]
